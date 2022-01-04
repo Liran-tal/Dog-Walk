86b9962eb9f5d923d70ba76cb4167355f37510c1
Liran-tal <taliran44@gmail.com>
2022-01-04 18:28:45 +0200

    styling navbar component

--- src/components/NavBar/NavBar.component.jsx
+++ src/components/NavBar/NavBar.component.jsx
@@ -5,42 +5,42 @@ import { AiFillStar } from "react-icons/ai"
 import { FiUser } from "react-icons/fi"
 import { BiSearchAlt2 } from "react-icons/bi"
 import { UserContext } from "../UserContext/UserContext";
-
+import "./NavBar.style.css"
 
 export default function NavBar() {
 
-	const {user, setUser} = useContext(UserContext);
+	const {user} = useContext(UserContext);
 
 	return (
-		<div >
+		<div className="Navbar Navbar-flex-row" >
 			<Link to="/" >
-				<div>
-					<AiTwotoneHome />
-					<div>
+				<div className="Navbar-flex-row" >
+					<AiTwotoneHome className="Navbar-link-icon" />
+					<div className="Navbar-link-text" >
 						בית
 					</div>
 				</div>
 			</Link>
 			<Link to={user ? "/user" : "/login"} >
-				<div >
-					<FiUser />
-					<div>
+				<div className="Navbar-flex-row" >
+					<FiUser className="Navbar-link-icon" />
+					<div className="Navbar-link-text" >
 						{user ? <span>{user.name}</span> : <span>התחברות</span>}
 					</div>
 				</div>
 			</Link>
 			<Link to="/favorites" >
-				<div>
-					<AiFillStar />
-					<div>
+				<div className="Navbar-flex-row" >
+					<AiFillStar className="Navbar-link-icon" />
+					<div className="Navbar-link-text" >
 						מועדפים
 					</div>
 				</div>
 			</Link>
 			<Link to="/search" >
-				<div>
-					<BiSearchAlt2 />
-					<div>
+				<div className="Navbar-flex-row" >
+					<BiSearchAlt2 className="Navbar-link-icon" />
+					<div className="Navbar-link-text" >
 						חיפוש
 					</div>
 				</div>
